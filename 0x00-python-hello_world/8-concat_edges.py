672144094aad9296abcb77082dcd19c3fe831a8d
princessble <kwomofortuneb@gmail.com>
2023-06-05 12:22:54 -0700

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
-string = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-substring1 = string[38:66]
-substring2 = string[106:111]
-substring3 = string[:6]
-result = substring1 + substring2 + substring3
-print(result)
+str = "Python is an interpreted, interactive, object-oriented programming
+        language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
