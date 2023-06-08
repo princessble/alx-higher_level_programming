70830e30c181882a717925c32c009411fab68407
princessble <kwomofortuneb@gmail.com>
2023-06-08 14:28:27 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
-def add(a, b):
-    """Add two numbers."""
-    return a + b
+from add_0 import add
 
 if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
