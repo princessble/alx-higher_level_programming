b15dcf92c6dc2adf9269c3bc6189d07b44f7e3e7
princessble <kwomofortuneb@gmail.com>
2023-06-08 14:24:39 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+
 def add(a, b):
-    """My addition function
+    """Add two numbers."""
+    return a + b
 
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
