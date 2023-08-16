f1457c432da3736626a4c72c8c7a70a052ce696b
princessble <kwomofortuneb@gmail.com>
2023-08-16 15:12:58 -0700

    upadte 1-create_user.sql file

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,10 @@
--- Creates and diplays the user user_0d_1 with all privileges.
--- Creates the MySQL user user_0d_1 and grants them all privileges.
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
 
