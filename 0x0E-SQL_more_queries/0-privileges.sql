cf076af6e9b031232671a4fd2609d55973805661
princessble <kwomofortuneb@gmail.com>
2023-08-16 14:05:42 -0700

    upadate 0-privileges .sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,6 @@
--- lists all privileges of the MySQL users
--- on my server
+-- My privileges
+-- show privs for user1
 SHOW GRANTS FOR user_0d_1@localhost;
+-- show privs for user2
 SHOW GRANTS FOR user_0d_2@localhost;
 
