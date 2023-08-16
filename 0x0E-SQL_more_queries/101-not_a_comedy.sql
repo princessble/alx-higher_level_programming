9ea25a49925658c6d429eea6fc1a51b019122782
princessble <kwomofortuneb@gmail.com>
2023-08-16 14:59:46 -0700

    Records are sorted in ascending order by show titles

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,20 @@
+-- Enumerates all shows within the hbtn_0d_tvshows database that do not have the comedy genre.
+-- Records are sorted in ascending order by show titles.
+SELECT DISTINCT `title`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON s.`show_id` = t.`id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE t.`title` NOT IN
+             (SELECT `title`
+                FROM `tv_shows` AS t
+	             INNER JOIN `tv_show_genres` AS s
+		     ON s.`show_id` = t.`id`
+
+		     INNER JOIN `tv_genres` AS g
+		     ON g.`id` = s.`genre_id`
+		     WHERE g.`name` = "Comedy")
+ ORDER BY `title`;
+
