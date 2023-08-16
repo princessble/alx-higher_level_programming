417edbc03bfb330340d8f59bd602d9b746c487b4
princessble <kwomofortuneb@gmail.com>
2023-08-16 14:21:33 -0700

    creates the states table.file for 6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Sets up the hbtn_0d_usa database and creates the states table.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
+);
+
