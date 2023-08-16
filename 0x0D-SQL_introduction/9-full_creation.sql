ab2a1b495cf9e65d9a6fb7abeb07de176bb69481
princessble <kwomofortuneb@gmail.com>
2023-08-16 11:44:30 -0700

    update 9-full_creation.sql file

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Create a table and insert multiple records into it.
--- Execute: cat 9-full_creation.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+-- Script should create maximum of 4 records
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
