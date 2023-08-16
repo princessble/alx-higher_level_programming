8880f81dbe34f5a5d9d00fd7b6cf81f454659366
princessble <kwomofortuneb@gmail.com>
2023-08-16 14:53:54 -0700

    Records and Enumerates all shows and their linked genres from the hbtn_0d_tvshows database.

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- Enumerates all shows and their linked genres from the hbtn_0d_tvshows database.
+-- Records are sorted in ascending order of show titles and genre names.
+SELECT t.`title`, g.`name`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON s.`genre_id` = g.`id`
+ ORDER BY t.`title`, g.`name`;
+
