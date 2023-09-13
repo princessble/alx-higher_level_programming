ce7b8aa9686d76401f995a534485bda62bfc1628
princessble <kwomofortuneb@gmail.com>
2023-09-13 11:45:18 -0700

    feat: Implement script to print 3 lines in JavaScript

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Use console.log(...) to print the first line
+console.log("C is fun");
+
+// Use console.log(...) to print the second line
+console.log("Python is cool");
+
+// Use console.log(...) to print the third line
+console.log("JavaScript is amazing");
+
