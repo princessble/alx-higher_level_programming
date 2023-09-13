32d3cf5a0635e1c3555f82dbcf59a5b9b830fb8b
princessble <kwomofortuneb@gmail.com>
2023-09-13 12:14:52 -0700

    update 3- value argument .js file

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
+
 // Check if exactly two arguments are provided
 if (process.argv.length !== 4) {
   console.error("Usage: ./4-concat.js <arg1> <arg2>");
   process.exit(1);
 }
+
 // Extract the two arguments
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
+
 // Use console.log(...) to print the formatted output
 console.log(arg1 + " is " + arg2);
 
